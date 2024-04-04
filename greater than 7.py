1be95db1fbe6803749377dacc2c19218fea880f4
aboutmeinpxls <162509128+aboutmeinpxls@users.noreply.github.com>
2024-04-04 12:33:21 +0200

    Update greater than 7.py
    If the entered number is greater than 7, then print “Hello”

--- greater than 7.py
+++ greater than 7.py
@@ -1,4 +1,17 @@
 
-num = int(input("Enter an number as input: "))
-if num>7:
-  print("hello")
+def check_number(num):
+    try:
+        num = int(num)
+        if num > 7:
+            print("Hello")
+    except ValueError:
+        print("Please enter a valid integer.")
+
+# Continuously ask for input until user enters "exit"
+while True:
+    user_input = input("Enter a number (or 'exit' to stop): ")
+    if user_input.lower() == 'exit':
+        break
+    else:
+        check_number(user_input)
+
